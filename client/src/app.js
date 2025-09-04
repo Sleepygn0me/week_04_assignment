705c80d3edb24da1d2bfa7fc6d9de050ed5ef9cd
Richard Thornton <richt9662@gmail.com>
2025-09-04 14:09:16 +0100

    feat: added the feature for the user to submit new data to the database

--- client/src/app.js
+++ client/src/app.js
@@ -6,13 +6,23 @@ const gamesForm = document.getElementById("games-form");
 //add submit even to the DOM
 gamesForm.addEventListener("submit", handleSubmit);
 
-//-add and event to this html element
-
-//-the event handler has the following steps:
-//    -prevent default behaviour of the even
-//    -create template for the userser data
-//    - fill in the template with usere;s data
-//    -send data (JSON) to the server --> while in development the server url uses localhost, but when you finish deploying and your project is don you need to replace the localhost url with the server deployed url.
+function handleSubmit(event) {
+  event.preventDefault();
+  //create a newtemplate object using form inputs
+  const formData = new FormData(gamesForm);
+  //fill the input data in the template object
+  const formValues = Object.fromEntries(formData);
+  console.log(formValues);
+  // send the data to the server
+  fetch("http://localhost:8080/add-games", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ formValues }),
+  });
+}
+//    -send data (JSON) to the server --> while in development the server url uses localhost, but when you finish deploying and your project is done you need to replace the localhost url with the server deployed url.
 
 //todo: get database data from server
 // -connect our client with the specific server route that GETs my data
